8dec196260f908e04b419bd35d8f5b7c24fed7b7
Jon B <jonburger@gmail.com>
2019-09-03 09:54:16 +0100

    v0.1.1

--- src/cli.js
+++ src/cli.js
@@ -1,40 +1,51 @@
 export function cli(args) {
     const opts = process.argv.slice(2);
-    let nums = [];
+    let runs = /^\d+$/.test(opts[0]) ? Math.max(1, parseInt(opts.shift())) : 1;
 
-    opts.forEach(arg => {
-        let match = /(?<count>\d+):(?<total>\d+)/.exec(arg);
+    if (opts.length === 0) {
+        console.error('\x1b[31m%s\x1b[0m', `Arguments must be provided in the format <count>:<maxvalue>`);
+        return;
+    }
 
-        if (!match) {
-            console.error('\x1b[31m%s\x1b[0m', `Argument "${arg}" must integers in the format <count>:<maxvalue>`);
-            return;
-        }
+    while (runs--) {
+        let nums = [];
 
-        let count = parseInt(match.groups.count);
-        let total = parseInt(match.groups.total);
+        opts.forEach(arg => {
+            const match = /^(?<count>\d+):(?<total>\d+)$/.exec(arg);
 
-        if (isNaN(count) || count < 1 || isNaN(total) || total < 1) {
-            console.error('\x1b[31m%s\x1b[0m', `Argument "${arg}" must integers in the format <count>:<maxvalue>`);
-            return;
-        }
+            if (!match) {
+                console.error('\x1b[31m%s\x1b[0m', `Argument "${arg}" must be integers in the format <count>:<maxvalue>`);
+                return;
+            }
 
-        let balls = Array(total).fill().map((item, i) => i + 1);
-        let lucky = [];
+            const count = parseInt(match.groups.count);
+            const total = parseInt(match.groups.total);
 
-        for (let i = 0; i < count; i++) {
-            lucky.push(shuffle(balls).pop());
-        }
+            if (isNaN(count) || count < 1 || isNaN(total) || total < 1) {
+                console.error('\x1b[31m%s\x1b[0m', `Argument "${arg}" must be integers in the format <count>:<maxvalue>`);
+                return;
+            }
 
-        nums.push(lucky.join(' '));
-    });
+            const balls = Array(total).fill().map((item, i) => (i + 1 + '').padStart(2, '0'));
+            const lucky = [];
 
-    console.log('\x1b[32m%s\x1b[0m', nums.join('\x1b[0m \x1b[2m-\x1b[0m \x1b[32m'));
+            for (let i = 0; i < count; i++) {
+                lucky.push(shuffle(balls, 10).pop());
+            }
+
+            nums.push(lucky.join(' '));
+        });
+
+        console.log('\x1b[32m%s\x1b[0m', nums.join('\x1b[0m \x1b[2m-\x1b[0m \x1b[32m'));
+    }
 }
 
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
+function shuffle(arr, count = 1) {
+    for (let k = 0; k < count; k++) {
+        for (let i = arr.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
     }
-    return a;
+    return arr;
 }
